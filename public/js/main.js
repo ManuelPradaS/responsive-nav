1d5d2e19b6e0b3460a72862211f344db1ad9a0a5
MPrada <manuel.prada@endava.com>
2019-02-13 16:50:20 -0500

    NavBar v1.5

--- public/js/main.js
+++ public/js/main.js
@@ -31,6 +31,7 @@ fetch('http://localhost:3000/api/nav.json')
         let currentLi =document.createElement("li");
         currentLi.setAttribute("class",classLi);
 
+
         let currentChild =document.createElement("a");
 
         currentChild.innerHTML=currentItem.label;
@@ -47,6 +48,7 @@ fetch('http://localhost:3000/api/nav.json')
 
       return listNavBar;
     }
+
 
 
 
