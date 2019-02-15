9966f10eb3de35c9e93e4a85cfedd4f3fd3c4aa8
Alejandro Cano <Alejandro.Cano@endava.com>
2019-02-14 20:07:50 -0500

    Yendo a casa

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,91 @@
 // JS Here!
 fetch('http://localhost:3000/api/nav.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
+        console.log(myJson);
+        //Call nav bar
+        const navBar = document.getElementById("navBar");
 
-    const navBar = document.getElementById("navBar");
-    navBar.appendChild(addItems(myJson,"menu","menuItem"));
+        //Append the menu to navBar
+        navBar.appendChild(addItems(myJson, "menu", "menuItem"));
 
 
-    function addItems(data,classUl,classLi){
+        function addItems(data, classUl, classLi) {
 
-      const listNavBar = document.createElement("ul");
-      listNavBar.setAttribute("class",classUl);
+            const menuList = document.createElement("ul");
+            menuList.setAttribute("class", classUl);
 
-      data.items.forEach(currentItem => {
+            data.items.forEach(currentItem => {
 
-        let currentLi = document.createElement("li");
-        currentLi.setAttribute("class",classLi);
+                //Create current menuitem
+                let currentLi = document.createElement("li");
+                currentLi.setAttribute("class", classLi);
+                // currentLi.setAttribute("label", currentItem.label)
 
-        let currentChild = document.createElement("a");
+                //Crate a link
+                let currentChild = document.createElement("a");
+                currentChild.innerHTML = currentItem.label;
+                currentChild.setAttribute("href", currentItem.url);
 
-        currentChild.innerHTML=currentItem.label;
-        currentChild.setAttribute("href",currentItem.url);
-        currentLi.appendChild(currentChild);
-        listNavBar.appendChild(currentLi);
 
-        if(currentItem.items!== undefined&&currentItem.items.length){
-          currentChild.appendChild(addItems(currentItem,"dropDown","dropDownItem"));
+                if (currentItem.items !== undefined && currentItem.items.length) {
+                    currentChild.appendChild(addChildDropDown(currentItem, "dropDownMenu"))
+                    //Add currentChild to currentLi
+                    //Create a button, and add to the menu.
+                    const thisButton = document.createElement("button");
+                    thisButton.setAttribute("onclick", "dropDownMenu()");
+                    thisButton.setAttribute("class", "dropButton");
+                    currentLi.appendChild(thisButton);
+                }
+
+
+                currentLi.appendChild(currentChild);
+
+                //Add currentLi to menu list
+                menuList.appendChild(currentLi);
+
+
+            });
+
+            return menuList;
         }
 
+        function addChildDropDown(data, divName) {
 
-      });
+            //Div for contain the dropDownMenu
+            const dropDown = document.createElement("div");
+            dropDown.setAttribute("class", divName);
 
-      return listNavBar;
-    }
+            // thisButton.setAttribute("tag", "Dropdown");
+            // thisButton.innerHTML = data.label;
 
 
+            //Create a div to contain the items
+            const content = document.createElement("div");
+            content.setAttribute("class", "dropdown-content");
+            content.setAttribute("id", "myDropdown");
 
-  });
+            data.items.forEach(currentItem => {
+
+                let currentChild = document.createElement("a");
+                currentChild.setAttribute("href", currentItem.url);
+                currentChild.innerHTML = currentItem.label;
+                content.appendChild(currentChild);
+
+
+            })
+            dropDown.appendChild(content);
+
+            return dropDown;
+        }
+    
+    function dropDownMenu() {
+
+    document.getElementById("myDropdown").classList.toggle("show"); }
+
+    });
+
 
 
